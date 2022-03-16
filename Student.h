9badccb7817edb3ffe528392e19682016f7b1a31
Gurusinghe <99312324+IT21298080@users.noreply.github.com>
2022-03-16 05:39:28 +0000

    Completed

--- Student.h
+++ Student.h
@@ -1,9 +1,17 @@
-class Student {
+
   // private section
+class Student {
+    private:
   //    int studentId
+       int studentID;
   //    name <- 20 charcters
+       char studentName[20];
   
   // public section
+    public:
+
   //    assignDetails() method declaration
+        void assignDetails(int no,char name[]);
   //    display() method declaration
+        void Display();
 };
